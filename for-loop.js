a5a0a8863beafcfd654fd0270ff6c789add3a4c7
habib.codes <habib.codes@gmail.com>
2021-08-04 10:49:42 -0400

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,7 @@
+let total = 0;
+limit = 10;
+
+for (i = 0; i < limit; i++) {
+  total += 1;
+}
+console.log(total);
---
+++ object-keys.js
@@ -0,0 +1,9 @@
+let car = {
+  make: 'Honday',
+  model: 'Accord',
+  year: 2020,
+};
+
+let keys = Object.keys(car);
+
+console.log(keys);
